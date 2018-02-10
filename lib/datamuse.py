6e6005d4f8479274af2fae77b673e27e5c8037cf
tornado711 <spartan51>
2018-02-10 13:05:48 -0500

    Adds docstring documentation fo rthe rhyme and synonym functions

--- lib/datamuse.py
+++ lib/datamuse.py
@@ -10,6 +10,16 @@ import os
 import json
 import requests
 
+
+"""Get a synonym of an inputted word from the muse API
+
+Keyword arguments:
+word - the word to find synonyms for
+
+return:
+A list of strings that rhyme with the word
+"""
+
 def rhyme(word):
     url = "https://api.datamuse.com/words?rel_rhy="
     response = requests.get(url + word)
@@ -19,6 +29,32 @@ def rhyme(word):
         rhyme_list.append(rhyme["word"])
     return " ".join(rhyme_list)
 
+
 rhyming_word = os.environ['RHYMING_WORD']
 rhymes = rhyme(rhyming_word)
 print(rhymes)
+
+"""Get a synonym of an inputted word from the muse API
+
+Keyword arguments:
+word - the word to find synonyms for
+
+return:
+A list of strings that are synonymous with the word
+"""
+def synonym(word):
+    url="https://api.datamuse.com/words?ml="
+    response = requests.get(url + word)
+    synonyms = json.loads(response.text);
+    synonym_list = []
+    for synonym in synonyms[0:8]:
+        synonym_list.append(synonym["word"])
+        return " ".join(synonym_list)
+
+
+"""Set the environment variables and print the synonym list."""
+synonymous_word = os.environ['SYNONYMOUS_WORD']
+synonyms = synonym(synonymous_word)
+print(synonyms)
+
+
