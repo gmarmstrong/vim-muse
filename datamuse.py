23e4485f4814dfaf268402bb0a7165b5824ca07e
gmarmstrong <guthrie.armstrong@gmail.com>
2018-02-09 22:02:29 -0500

    Create datamuse.py

---
+++ datamuse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# DataMuse API documentation: https://www.datamuse.com/api/
+# DataMuse API root: https://api.datamuse.com/
+
+# This is a Python library that acts as a wrapper for the DataMuse API's rhyme
+# function.
+
+import json
+import requests
+
+def rhyme(word):
+    url = "https://api.datamuse.com/words?rel_rhy="
+    response = requests.get(url + word)
+    rhymes = json.loads(response.text)
+    rhyme_list = []
+    for rhyme in rhymes:
+        rhyme_list.append(rhyme["word"])
+    return rhyme_list
