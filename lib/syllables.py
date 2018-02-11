40215d9311298364411a6955ee7627a61cfc8f9f
gmarmstrong <guthrie.armstrong@gmail.com>
2018-02-11 02:38:42 -0500

    Use nltk tokenization to prepare to syllables

--- lib/syllables.py
+++ lib/syllables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import string
+import nltk
 from nltk.corpus import cmudict
 
 dictionary = cmudict.dict()
@@ -8,22 +8,19 @@ dictionary = cmudict.dict()
 # Count syllables in a word
 def nsyl_word(word):
     # See: https://stackoverflow.com/a/4103234/6791398
-    return [len(list(y for y in x if y[-1].isdigit())) for
-            x in dictionary[word.lower()]]
+    return [len(list(y for y in x if y[-1].isdigit())) for x in dictionary[word.lower()]]
 
 # Count syllables in a sentence
 def nsyl_sentence(sentence):
-    for word in sentence.split():
-        if word.lower() not in dictionary:
-            print("Typo in word: " + word + ". Aborting.")
-            quit()
+    tokens = nltk.word_tokenize(sentence)
 
-    # Strip punctuation
-    sentence = "".join((char for char in sentence if
-        char not in string.punctuation))
+    for token in tokens:
+        if token.lower() not in dictionary:
+            print("Typo in word: " + token + ". Aborting.")
+            quit()
 
     # Count syllables
     count = 0
-    for word in sentence.split():
-        count += nsyl_word(word)[0]
+    for token in tokens:
+        count += nsyl_word(token)[0]
     return count
