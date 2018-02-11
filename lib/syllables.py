20882b771c1847c7556c1abbf8be10932349d444
gmarmstrong <guthrie.armstrong@gmail.com>
2018-02-11 02:28:27 -0500

    Abort syllable count on typo

--- lib/syllables.py
+++ lib/syllables.py
@@ -3,14 +3,21 @@
 import string
 from nltk.corpus import cmudict
 
+dictionary = cmudict.dict()
+
 # Count syllables in a word
 def nsyl_word(word):
     # See: https://stackoverflow.com/a/4103234/6791398
-    d = cmudict.dict()
-    return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
+    return [len(list(y for y in x if y[-1].isdigit())) for
+            x in dictionary[word.lower()]]
 
 # Count syllables in a sentence
 def nsyl_sentence(sentence):
+    for word in sentence.split():
+        if word.lower() not in dictionary:
+            print("Typo in word: " + word + ". Aborting.")
+            quit()
+
     # Strip punctuation
     sentence = "".join((char for char in sentence if
         char not in string.punctuation))
