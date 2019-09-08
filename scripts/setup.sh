319c8ac09ce1d1c8802db49c612fa0f9b3ef1cc3
gmarmstrong <guthrie.armstrong@gmail.com>
2019-09-08 19:28:23 -0400

    Fix Travis permissions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pip3 install python-datamuse
-sudo pip3 install nltk
+sudo pip3 install --upgrade setuptools
+sudo pip3 install --upgrade python-datamuse
+sudo pip3 install --upgrade nltk
 python3 -c "import nltk; nltk.download('cmudict')"
