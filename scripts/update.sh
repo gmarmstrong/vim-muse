ddf3a66b32044f4f866fd5a5ed572fe9701692bb
gmarmstrong <guthrie.armstrong@gmail.com>
2019-09-08 19:53:14 -0400

    Use vim batch mode so vader can use stderr

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo pip3 install --upgrade setuptools
 sudo pip3 install python-datamuse --upgrade
 sudo pip3 install nltk --upgrade
 python3 -c "import nltk; nltk.download('cmudict')"
